57b2eebbc0fa84b160d2719fcd2dab03eb029db6
Kevin <kevincarrabine23@gmail.com>
2022-07-12 21:08:15 -0400

    wip

--- bundle.js
+++ bundle.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var version = "1.0.0";
 
 function main () {
-  console.log('version: ', version);
+  console.log("version: ", version);
 }
 
-export { main as default };
-//# sourceMappingURL=bundle.js.map
+module.exports = main;
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,29 @@
-import json from '@rollup/plugin-json'
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+
+// plugin
+
+// Bundles code
+
+// Looks for a specific file main.js
+
+// changes the content of the file
+
+// re-builds the bundle
 
 export default {
-    input: 'main.js',
-    output: {
-        file: 'bundle.js',
-        format: 'es',
-        sourcemap: true
+  input: "main.js",
+  output: [
+    {
+      file: "bundle.js",
+      format: "cjs",
     },
-    plugins: [json()]
-}
+    {
+      file: "bundle.min.js",
+      format: "iife",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [json()],
+};
